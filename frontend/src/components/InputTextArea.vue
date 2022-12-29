720a73d0f20e94d29670ac431ce8113ce796cf65
Seppe Dijkmans <seppedijkmans@outlook.com>
2022-12-29 20:29:48 +0100

    Started on the text area

---
+++ frontend/src/components/InputTextArea.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  defaultText: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <textarea class="text-input"/>
+</template>
+
+<style scoped>
+
+.text-input {
+  height: 2rem;
+  width: 100vh;
+  height: 50vh;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ frontend/src/components/partials/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: false
+    },
+
+    onClick: {
+        type: Function,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <button class="button" />
+</template>
+
+<style scoped>
+.text-input {
+    height: 2rem;
+    width: 100vh;
+    height: 50vh;
+}
+
+@media (min-width: 1024px) {
+
+    .greetings h1,
+    .greetings h3 {
+        text-align: left;
+    }
+}
+</style>
