0b0b6877afd81d8da5d65b8216579006639109a0
Seppe Dijkmans <seppedijkmans@outlook.com>
2023-01-02 19:35:26 +0100

    Push backend that can do synonyms

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,5 +1,8 @@
 import spacy;
 from spacy.lang.nl.examples import sentences
+from OpenDutchWordnet import Wn_grid_parser
+from flask import jsonify
+
 from collections import Counter
 
 nlp = spacy.load("nl_core_news_md")
@@ -23,10 +26,19 @@ def extract_most_used_lemmas_and_pos(doc):
 
     return { "pos_per_sentence": pos_per_sentence, "lemmas": lemmas}        
 
+
 def process_text(text):
-    print("Hi guys, im here" + text)
     doc = nlp(text);
+    instance = Wn_grid_parser(Wn_grid_parser.odwn);
     result = extract_most_used_lemmas_and_pos(doc);
     most_occurring_pos = Counter(result["pos_per_sentence"]).most_common(3)
     most_occuring_lemmas = Counter(result["lemmas"]).most_common(3)
-    return {"most_occurring_pos": most_occurring_pos, "most_occurring_lemmas": most_occuring_lemmas}
+    most_occuring_lemmas_with_synonyms = []
+
+    for lemma in most_occuring_lemmas:
+        synonyms = instance.les_lemma_synonyms(lemma[0])
+        most_occuring_lemmas_with_synonyms.append({"lemma": lemma[0], "occurences": lemma[1], "synonyms": list(synonyms)})
+        
+    print(most_occuring_lemmas_with_synonyms);
+
+    return {"most_occurring_pos": most_occurring_pos, "most_occurring_lemmas": most_occuring_lemmas_with_synonyms}
