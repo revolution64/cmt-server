a39920de2226d1c52299661a5bf644b9335fa1d2
Seppe Dijkmans <seppedijkmans@outlook.com>
2023-01-05 19:21:33 +0100

    Added default route, removed black screen preference and remove localhost link

--- frontend/src/api/cmt.js
+++ frontend/src/api/cmt.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
-export const analyzeText = (text) => axios.post(`http://localhost:5000`, {
+function isLocalHost(url) {
+    return url.indexOf('localhost') !== -1 || url.indexOf('127.0.0.1') !== -1;
+}
+
+function getHost() {
+    isLocalHost ? 'http://localhost:5000' : 'https://revolution64.pythonanywhere.com'
+}
+
+export const analyzeText = (text) => axios.post(getHost(window.location.host), {
     "text": text
 });
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'other',
+      component: HomeView
     }
   ]
 })
