4f8831389b982d8bcb5156ef124ac0c91a55c8bc
Seppe Dijkmans <seppedijkmans@outlook.com>
2023-01-16 21:04:08 +0100

    try to improve synonyms

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,7 +1,9 @@
 import spacy;
+import json;
 from OpenDutchWordnet import Wn_grid_parser
 from collections import Counter
 
+instance = Wn_grid_parser(Wn_grid_parser.odwn)
 nlp = spacy.load("nl_core_news_md")
 
 def lemmatizeAndPosTagDoc(text):
@@ -18,21 +20,28 @@ def lemmatizeAndPosTagDoc(text):
             pos_per_sentence.append(sentence[1:])
             sentence = ""
         elif any(token.pos_ in pos for pos in ["NOUN", "VERB", "ADJ"]):
-            lemmas.append(token.lemma_)
+            print(token.lemma_ + "-" + token.pos_[0].lower() + "-1")
+            lemmas.append(token.lemma_ + "-" + token.pos_[0].lower() + "-1")
 
     return { "pos_per_sentence": pos_per_sentence, "lemmas": lemmas}        
 
 
-def getSynonyms(most_occuring_lemmas):
+def getSynonyms(most_occuring_lemmas = []):
     most_occuring_lemmas_with_synonyms = []
-    instance = Wn_grid_parser(Wn_grid_parser.odwn)
 
     for lemma in most_occuring_lemmas:
-        synonyms = list(instance.les_lemma_synonyms(lemma[0]))[0:10]
-        if lemma[0] in synonyms:
-            synonyms.remove(lemma[0])
+        lemmaWithPOS = lemma[0]
+        le_el = instance.les_find_le(lemmaWithPOS)
+        
+        if le_el is not None:
+            synset_el = instance.synsets_find_synset(le_el.get_synset_id())
+            print(synset_el.get_glosses(['nl']))
+
+        synonyms = list(instance.les_lemma_synonyms("fiets"))[0:10]
+        if "fiets" in synonyms:
+            synonyms.remove("fiets")
         most_occuring_lemmas_with_synonyms.append(
-            {"lemma": lemma[0], "occurences": lemma[1], "synonyms": synonyms})
+            {"lemma": "fiets", "occurences": lemma[1], "synonyms": synonyms})
 
     return most_occuring_lemmas_with_synonyms
 
